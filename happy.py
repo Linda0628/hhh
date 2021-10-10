5dac991f2ace0a284f0956c0a72d6b369a3b5b4a
Linda0628 <91074814+Linda0628@users.noreply.github.com>
2021-10-10 21:36:53 +0100

    Update happy.py
    try 'PULL'

--- happy.py
+++ happy.py
@@ -8,3 +8,5 @@ college = 'UoE and UCC'
 
 print(f'Hi, my name is {name}, and I am {age} years old, my birthday is {birthday}')
 print(f'I love {lover}, my college is {college}')
+
+print('just try how to PULL it into VScode')
