8077fcb6cf78066df1c5b1c3646997b71618f70e
Linda0628 <91074814+Linda0628@users.noreply.github.com>
2021-10-10 21:37:45 +0100

    Update happy.py
    pull ir using terminal

--- happy.py
+++ happy.py
@@ -10,3 +10,5 @@ print(f'Hi, my name is {name}, and I am {age} years old, my birthday is {birthda
 print(f'I love {lover}, my college is {college}')
 
 print('just try how to PULL it into VScode')
+
+print('just try how to PULL it into VScode from terminal')
