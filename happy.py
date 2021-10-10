a0334caa24b35a4c2814b6cc05ddfcf11d87b3ee
Linda0628 <s2190770@ed.ac.uk>
2021-10-10 21:25:55 +0100

    added my lover

--- happy.py
+++ happy.py
@@ -1,4 +1,8 @@
 name = 'Bowen Zhang'
 age = 24
 birthday = '27th September'
-print(f'Hi, my name is {name}, and I am {age} years old, my birthday is {birthday}')
+
+lover = 'Linda'
+
+print(f'Hi, my name is {name}, and I am {age} years old, my birthday is {birthday},I love {lover}')
+
