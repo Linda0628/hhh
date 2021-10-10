799a4cf5b98f9105c74c573fcc35f88be7f9c401
Linda0628 <s2190770@ed.ac.uk>
2021-10-10 21:30:51 +0100

    added my university

--- happy.py
+++ happy.py
@@ -4,5 +4,7 @@ birthday = '27th September'
 
 lover = 'Linda'
 
-print(f'Hi, my name is {name}, and I am {age} years old, my birthday is {birthday},I love {lover}')
+college = 'UoE and UCC'
 
+print(f'Hi, my name is {name}, and I am {age} years old, my birthday is {birthday}')
+print(f'I love {lover}, my college is {college}')
