abb08a43435ab7ead4efd42543b0c2d77eefff10
Linda0628 <s2190770@ed.ac.uk>
2021-10-10 21:50:00 +0100

    judge younger

--- happy.py
+++ happy.py
@@ -1,14 +1,20 @@
+## workshop 1 
 name = 'Bowen Zhang'
 age = 24
 birthday = '27th September'
-
 lover = 'Linda'
-
 college = 'UoE and UCC'
-
 print(f'Hi, my name is {name}, and I am {age} years old, my birthday is {birthday}')
 print(f'I love {lover}, my college is {college}')
-
 print('just try how to PULL it into VScode')
-
 print('just try how to PULL it into VScode from terminal')
+
+name_1 = 'ZBW'
+name_2 = 'Linda'
+age_1 = 24
+age_2 = 22
+birthday_1 = '10th April'
+birthday_2 = '28th June'
+judge = age_1 < age_2
+print(f'Is {name_1} younger than {name_2}? {judge}')
+
